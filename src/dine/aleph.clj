c8c536974e28aa4dc1b93a2febc0c5e17c1c0229
Oregu <oleg.burykin@gmail.com>
2014-06-28 20:35:30 +0400

    Getting started with aleph

--- src/dine/aleph.clj
+++ src/dine/aleph.clj
@@ -1,9 +1,24 @@
 (ns dine.aleph
   (:use [lamina.core]
-        [aleph.tcp]))
+        [aleph.tcp]
+        [gloss.core]))
 
-(defn table-handler [channel client-info]
-  (siphon channel channel))
+(defn table-handler [ch client-info]
+  (receive-all ch
+    #(enqueue ch (str "You said " %))))
+
+(def ch
+  (wait-for-result
+    (tcp-client {:host "localhost",
+                 :port 1984,
+                 :frame (string :utf-8 :delimiters ["\r\n"])})))
+
+(defn dine [myname]
+  (do
+    (enqueue ch "Hello, server!")
+    (wait-for-message ch)))
 
 (defn go []
-  (start-tcp-server table-handler {:port 1984}))
+  (let [folks [:Aristotle :Kant :Spinoza :Marx :Russel]]
+    (start-tcp-server table-handler {:port 1984, :frame (string :utf-8 :delimiters ["\r\n"])})
+    (map dine folks)))
